e5416317d25702e833536d855f3753fc69d59bb7
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-05-10 19:33:46 +0200

    changed copy

--- src/components/Header/BioText.js
+++ src/components/Header/BioText.js
@@ -5,8 +5,8 @@ export const BioText = () => {
     <div className="introduction">
       <p>
     As frontend developer, I am passionate about bridging the gap between technology and business.
-    Throughout my career, I have demonstrated my ability to translate complex business requirements
-    into intuitive and user-friendly solutions.
+    After spending years building new markets, I took a leap and learned how to code
+    - my dream is building products that customers love.
     With a strong foundation in sales and analytics,
     I bring a unique perspective to every project I embark on.
       </p>
