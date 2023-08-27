6175bc387805e87c3e2197a0c11c403392b25b3d
Sara Battilotti <sara.battilotti@gmail.com>
2023-08-27 15:02:29 +0200

    fix

--- src/components/Header/BioTitles.js
+++ src/components/Header/BioTitles.js
@@ -4,7 +4,7 @@ export const BioTitles = () => {
   return (
     <div className="info">
       <div className="name"> <h3> Sara Battilotti </h3> </div>
-      <div className="title"> <h3> Frontend Developer </h3> </div>
+      <div className="title"> <h3> Junior Developer </h3> </div>
       <div className="tag-line"> <h4> + Yoga Teacher </h4>  </div>
     </div>
   );
