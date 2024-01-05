d02c299339f509afadb9b63c6945d9bd13b9bc9b
Sara Battilotti <sara.battilotti@gmail.com>
2024-01-05 12:36:23 +0100

    bug fixes

--- src/components/Header/BioText.js
+++ src/components/Header/BioText.js
@@ -5,10 +5,10 @@ export const BioText = () => {
   return (
     <div className="introduction">
       <p>
-      I'm an enthusiastic Frontend Developer looking for a Junior/Mid role. I have a sales background in leading tech companies such as Zendesk & Segment. 
-      I love learning, I'm adaptable and have high attention to details. Thanks to my experience in remote work environments 
-      I know how to effectively manage projects and communicate in virtual settings. 
-      I offer a unique blend of technical and leadership skills that I'm excited to leverage in a new innovative project.
+      Enthusiastic Frontend Developer looking for a Junior/Mid role. I have a sales background in leading tech companies such as Zendesk & Segment.
+      I love learning, I am adaptable and have high attention to details. Thanks to my experience in remote work environments
+      I know how to effectively manage projects and communicate in virtual settings.
+      I offer a unique blend of technical and leadership skills that I am excited to leverage in a new innovative project.
       </p>
     </div>
   );
